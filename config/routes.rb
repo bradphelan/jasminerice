85bb93f1b9f080a1d2fb1b2f532021a7f7f433d6
Martin Provencher <mprovencher86@gmail.com>
2011-07-13 12:23:03 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Jasminerice::Engine.routes.draw do
   resources :spec, :controller => 'spec' do
-    get "fixtures/:filename"
+    get "fixtures/:filename", :action => :fixtures
   end
   root :to => "spec#index"
 end
