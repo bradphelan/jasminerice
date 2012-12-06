3992a1423f6873be3a1321510fc84ccc922d117c
Adam Bird <adam.bird@gmail.com>
2012-12-06 12:47:40 +0000

    Attempted to retain the warning

--- app/controllers/jasminerice/spec_controller.rb
+++ app/controllers/jasminerice/spec_controller.rb
@@ -1,6 +1,9 @@
 module Jasminerice
   class SpecController <  Jasminerice::ApplicationController
-  
+   warn "Using Jasminerice::HelperMethods is deprecated and will be removed in a future release,"\
+        "please use Jasminerice::SpecHelper to define your helpers in the future" if defined?(Jasminrice::HelperMethods)
+
+   helper Jasminerice::HelperMethods rescue nil
    helper Jasminerice::SpecHelper rescue nil
 
     layout false
