95ea68b82f2d0c871e7a70d33b8968a819f0d2a2
jejacks0n <jejacks0n@gmail.com>
2013-01-27 13:55:12 -0700

    shifts some logic from config into the engine.
    - moves asset paths into engine (removes the initializer).
    - moves mounting the routes into the engine and prepends them so they can't be clobbered with a glob route.
    
    I'm not positive about which version of Rails implemented the route.prepend method, but I think it's ok in 3.1+.

--- config/initializers/jasminerice.rb
+++
@@ -1,1 +0,0 @@
-Rails.application.config.assets.paths << Rails.root.join("spec", "javascripts") << Rails.root.join("spec", "stylesheets")
--- lib/jasminerice/engine.rb
+++ lib/jasminerice/engine.rb
@@ -1,5 +1,16 @@
 module Jasminerice
   class Engine < Rails::Engine
     isolate_namespace Jasminerice
+
+    initializer :assets, :group => :all do |app|
+      app.config.assets.paths << Rails.root.join("spec", "javascripts")
+      app.config.assets.paths << Rails.root.join("spec", "stylesheets")
+    end
+
+    config.after_initialize do |app|
+      app.routes.prepend do
+        mount Jasminerice::Engine => Jasminerice.mount_at
+      end if Jasminerice.mount
+    end
   end
 end
