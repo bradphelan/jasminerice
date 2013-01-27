ced290f0da9f12358d1bae2aa2612cbdb02bfbb5
Kyle Stevens <kstevens715@gmail.com>
2013-01-27 16:34:48 -0500

    Another small change.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Jasminerice::Engine.routes.draw do
   resources :spec, :controller => 'spec', :only => [:index] do
     get "fixtures/*filename", :action => :fixtures
   end
-  get "fixtures/*filename", :to => "spec#fixtures"
-  get "/(:suite)", :to => "spec#index", defaults: { suite: nil }
+  match "fixtures/*filename", :to => "spec#fixtures", :via => :all
+  match "/(:suite)", :to => "spec#index", :via => :all
 end
 
 if Jasminerice.mount
