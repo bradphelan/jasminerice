3ed77c0a30b2623ad0c60d3b70a369795193856f
Kyle Stevens <kstevens715@gmail.com>
2013-01-27 16:18:31 -0500

    Support Rails 4 style routing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Jasminerice::Engine.routes.draw do
   resources :spec, :controller => 'spec', :only => [:index] do
     get "fixtures/*filename", :action => :fixtures
   end
-  match "fixtures/*filename", :to => "spec#fixtures"
-  match "/(:suite)", :to => "spec#index", defaults: { suite: nil }
+  get "fixtures/*filename", :to => "spec#fixtures"
+  get "/(:suite)", :to => "spec#index", defaults: { suite: nil }
 end
 
 if Jasminerice.mount
