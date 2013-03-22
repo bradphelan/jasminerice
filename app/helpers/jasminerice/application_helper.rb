3a2e96046aaadc42749cf7d999e4b66c0cbdaeaf
Selva <k.n.selvakumar@gmail.com>
2013-03-22 22:42:20 +0530

    Support for code coverage with blanket.js

--- app/helpers/jasminerice/application_helper.rb
+++ app/helpers/jasminerice/application_helper.rb
@@ -1,4 +1,8 @@
 module Jasminerice
   module ApplicationHelper
+    def cover_with_blanket?(suite)
+      source_to_cover = "#{suite}_cover"
+      Rails.application.assets.find_asset(source_to_cover) ? source_to_cover : false
+    end
   end
 end
