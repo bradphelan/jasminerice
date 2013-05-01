b5370e3cf2931aa5478e775369afb5ecef1c18b5
jejacks0n <jejacks0n@gmail.com>
2013-05-01 10:35:46 -0600

    fixes issue when autoload isn't available. fixes #108

--- lib/generators/jasminerice/install_generator.rb
+++ lib/generators/jasminerice/install_generator.rb
@@ -1,15 +1,17 @@
 require 'rails'
 if ::Rails.version >= '3.1'
-  module Jasminerice::Generators
-    class InstallGenerator < ::Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+  module Jasminerice
+    module Generators
+      class InstallGenerator < ::Rails::Generators::Base
+        source_root File.expand_path('../templates', __FILE__)
 
-      def copy_files
-        copy_file 'jasminerice.rb', 'config/initializers/jasminerice.rb'
-        copy_file 'spec.js.coffee', 'spec/javascripts/spec.js.coffee'
-        copy_file 'example_spec.js.coffee', 'spec/javascripts/example_spec.js.coffee'
-        copy_file 'spec.css', 'spec/javascripts/spec.css'
-        copy_file 'example_fixture.html.haml', 'spec/javascripts/fixtures/example_fixture.html.haml'
+        def copy_files
+          copy_file 'jasminerice.rb', 'config/initializers/jasminerice.rb'
+          copy_file 'spec.js.coffee', 'spec/javascripts/spec.js.coffee'
+          copy_file 'example_spec.js.coffee', 'spec/javascripts/example_spec.js.coffee'
+          copy_file 'spec.css', 'spec/javascripts/spec.css'
+          copy_file 'example_fixture.html.haml', 'spec/javascripts/fixtures/example_fixture.html.haml'
+        end
       end
     end
   end
